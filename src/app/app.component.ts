6dace5de27d179cb7ed5b89db8387190dd310547
EshkerePro <mikola0672662963@gmail.com>
2020-07-23 15:24:05 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, ValidationErrors, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'less6DZ';
+
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      login: new FormControl('',
+         (passControl): ValidationErrors => {
+        if (passControl.value.length < 5) {
+          return {less: true};
+        }
+        if (passControl.value.length > 10) {
+          return  {ower: true};
+        }
+        }),
+      password: new FormControl(''),
+      repeatPass: new FormControl('')
+      },
+      this.passValid.bind(this)
+    );
+
+  }
+
+
+  save(form: FormGroup): void {
+    console.log(form.controls.login.value + '\n' + form.controls.password.value);
+  }
+
+passValid(form: FormGroup): null | object  {
+    const result1 = form.controls.password.value;
+    const  result2 = form.controls.repeatPass.value;
+
+    if (result2 === result1) {
+      return null;
+    }
+    else {
+      return {ErrPassword: true};
+    }
 }
+
+}
+
